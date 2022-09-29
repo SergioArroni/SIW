b61603f09847d9fc4987ef8709fba5dddf10cb39
Sergio <uo276341@uniovi.es>
2022-09-29 15:36:37 +0200

    Update entregado

--- Crawler.py
+++ Crawler.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 #=========================================================
-# Clase Crawler
 #
 # Autor: Sergio Arroni del Riego
 #
@@ -20,13 +19,18 @@ max_dw = 5
 ERROR_STATUS = -1
 
 def main(args):
-    """ Main program """
+    """main
+
+        Contiene toda la logica del Crawler
+
+        Params:
+            args, todos los argumentos dados por consola
+    """
     global max_dw
     
     semillas = args.file
     seconds=args.s
     max=args.m
-    name = args.name
     metodo = "a"
     semillas = parse(semillas)
     
@@ -42,10 +46,8 @@ def main(args):
     cont = 0
     urls = []
 
-    if metodo == "a":
-        urls = scan_a(semillas ,semillas[0], seconds)
-    else:
-        urls = scan_p(semillas[0] , seconds, semillas)
+
+    urls = scan(semillas[0] , seconds, semillas, metodo)
 
     if type(urls) == int and urls < 0:
         print("Ha ocurrido un error en la peticion, por favor consulte su red e intentelo otra vez :)") 
@@ -54,51 +56,20 @@ def main(args):
         f.write(f"\t{cont}\t{url}\n")
         cont += 1
     f.close()
+    
+def scan(url, seconds, semillas, metodo):
+    """scan
 
-def scan_p(url, seconds, semillas):
-    """ Main program """
-    # Code goes over here.
-    global max_dw
+        Escanea la pagina pasada como url, este metodo tiene la logica comun para anchura y profundidad
 
-    robots, seconds = robots_parser(url, seconds)
-    if not robots:
-        return urls
-    urls.append(url)
-
-    time.sleep(seconds)
-    html = request.get(url)
-
-    if not html.status_code==200:
-        return ERROR_STATUS
-
-    max_dw -= 1 
-    f = open(f'docs/Doc_{max_dw+1}.html', 'wb')
-    f.write(html.content)
-    f.close()
-
-    if max_dw == 0: 
-        return urls
-
-    html = bs4(html.text, 'html.parser')
-    links = html.find_all("a", href=True) 
-    for l in (links):
-        if max_dw == 0:
-            return urls
-
-        l = normalize_link(url, l.get('href'))
-
-        if l in urls:
-            continue
-        
-        semillas.insert(0, l)
-
-        scan_p(semillas[0], seconds, semillas)
-    return urls
-
-def scan_a(links_all , url, seconds):
-    """ Main program """
-    # Code goes over here.
-    global max_dw
+        Params:
+            url, la pagina que hay que escanear
+            seconds, segundos que hay que esperar entre peticiones
+            semillas, todos las urls que quedan por explorar
+            metodo, si es un escaneo en profundidad o en anchura
+    """
+    
+    global max_dw, urls
     robots, seconds = robots_parser(url, seconds)
     
     if not robots:
@@ -124,6 +95,55 @@ def scan_a(links_all , url, seconds):
 
     html = bs4(html.text, 'html.parser')
     links = html.find_all("a", href=True) 
+    
+    if metodo == "a":
+        urls = scan_a(semillas[0] , seconds, semillas, links, metodo)
+    else:
+        urls = scan_p(semillas[0] , seconds, semillas, links, metodo)
+        
+    return urls
+    
+       
+
+def scan_p(url, seconds, semillas, links, metodo ):
+    """scan_p
+
+        Escanea la pagina pasada como url, este metodo realiza la busqueda en profundidad de nuevas urls que escanear 
+
+        Params:
+            url, la pagina que hay que escanear
+            seconds, segundos que hay que esperar entre peticiones
+            semillas, todos las urls que quedan por explorar
+            links, lista con los links que se han encontrado en la url dada
+            metodo, si es un escaneo en profundidad o en anchura
+    """
+    for l in (links):
+        if max_dw == 0:
+            return urls
+
+        l = normalize_link(url, l.get('href'))
+
+        if l in urls:
+            continue
+        
+        semillas.insert(0, l)
+
+        scan(semillas[0], seconds, semillas, metodo)
+    return urls
+
+def scan_a(url, seconds, links_all, links, metodo ):
+    """scan_a
+
+        Escanea la pagina pasada como url, este metodo realiza la busqueda en anchura de nuevas urls que escanear 
+
+        Params:
+            url, la pagina que hay que escanear
+            seconds, segundos que hay que esperar entre peticiones
+            semillas, todos las urls que quedan por explorar
+            links, lista con los links que se han encontrado en la url dada
+            metodo, si es un escaneo en profundidad o en anchura
+    """
+    
     for l in (links):
         if max_dw == 0:
             return urls
@@ -136,10 +156,18 @@ def scan_a(links_all , url, seconds):
         links_all.pop(0)
         links_all.append(l)
 
-        scan_a(links_all ,links_all[0], seconds)
+        scan(links_all[0] ,seconds, links_all, metodo)
     return urls
 
 def normalize_link(url, link):
+    """normalize_link
+
+        Normaliza el link dado a la url 
+
+        Params:
+            url, la base de la url a normalizar
+            link, link a normalizar
+    """
     if link.startswith("/") or link.startswith("#"):
         return urljoin(url, link)
     return link
@@ -191,33 +219,34 @@ def parse(dir):
 
 def presentacion():
     """Presentacion
-
+    
     Funcion que muestra una presentacion en ASCII muy bonita
+    
     """
     
-    print(" ________  ________  ________  ___       __   ___       _______   ________     ")
-    print("|\   ____\|\   __  \|\   __  \|\  \     |\  \|\  \     |\  ___ \ |\   __  \    ")
-    print("\ \  \___|\ \  \|\  \ \  \|\  \ \  \    \ \  \ \  \    \ \   __/|\ \  \|\  \   ")
-    print(" \ \  \    \ \   _  _\ \   __  \ \  \  __\ \  \ \  \    \ \  \_|/_\ \   _  _\  ")
-    print("  \ \  \____\ \  \\  \\ \  \ \  \ \  \|\__\_\  \ \  \____\ \  \_|\ \ \  \\  \| ")
-    print("   \ \_______\ \__\\ _\\ \__\ \__\ \____________\ \_______\ \_______\ \__\\ _\ ")
-    print("    \|_______|\|__|\|__|\|__|\|__|\|____________|\|_______|\|_______|\|__|\|__|")                                                                        
-    print("Author: Sergio Arroni del Riego\n\n\n")
+    print("Crawler para la asignatura SIW :)\nAñade el argumento \"-h\" a la hora de ejecutar para optener la ayuda del Crawler")                                                                       
+    print("Autor: Sergio Arroni del Riego - UO276341\n\n\n")
 
 def parse_args():
     """Parse_args
+    
     Funcion que permite usar la consola para añadir parametros de una forma usable. 
+    
     """
     parser = argparse.ArgumentParser(description='WebCrawler para la asignatura SIW')
     parser.add_argument("--file", help="Fichero donde extraera las semillas de inicio. Default: semillas.txt",type=str, default="semillas.txt")
     parser.add_argument("--m", help="Numero maximo de descargar. Default: 10", type=int, default=10)
     parser.add_argument("--s", help="Numero de segundos que esperara entre cada iteracion. Default: 10", type=int, default=10)
-    parser.add_argument("--name", help="Nombre del Crawler. Default:TheBestCrawler", type=str, default="TheBestCrawler")
     parser.add_argument("-a", help="Tipo de exploracion que realizara: Anchura [a]. Default: true", action="store_true", default="true")
     parser.add_argument("-p", help="TTipo de exploracion que realizara: Profundidad [p]", action="store_true")
     args = parser.parse_args()
     return args
 
 if __name__ == "__main__":
+    """__main__
+    
+    Primero imprime la presentacion del Crawler y luego el main con la logica del mismo
+    
+    """
     presentacion()
     main(parse_args())
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # SIW
-Repo para la asignatura de SIW
+
+## Crawler P2
+He realizado los dos opcionales tanto la busqueda en profundidad y anchura, como el fichero de robots.txt
+
+- Para ejecutar el archivo se le pasan los parametros por consola, si pulsas -h te sacara la ayuda, pero en resumen:
+    ```
+        -p  Busqueda en profundidad
+        -a  Busqueda en anchura  
+        --m  Maximos docs a descargar
+        --s  Segundos entre peticiones
+        --file Ruta archivo de semillas
+    ```
+- El fichero semillas.txt son las semillas que use yo y los dos .txt de resultados son para 10 archivos descargados.
+
+- La carpeta docs es donde se descargan los htmls y se guardan las urls en las que busco. 
+
+ 
