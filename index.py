a8e58653af202e4f333ded7f08f1a234477ecbe9
Sergio <uo276341@uniovi.es>
2022-10-21 12:24:06 +0200

    Actu 1

---
+++ index.py
@@ -0,0 +1,129 @@
+import json
+import math
+import pickle
+from re import I
+from typing import Counter
+import bag_og_words as bag_w
+import argparse
+
+
+class Index:
+
+    def __init__(self, args: argparse.Namespace) -> None:
+        self.filename = args.file
+        self.index = {}
+        self.load_data()
+        self.n_total = 0
+
+    def load_data(self) -> dict:
+        """load_data
+
+            Funcion que carga los datos de un fichero
+
+        Parameters:
+            None
+        Output: 
+            dict
+        """
+        text_aux = ""
+        self.train_list = {}
+        train = open(self.filename, "r")
+        for i in train:
+            i = i.replace('\n', "")
+            i = i.replace('\t', "")
+            if i.isdigit():
+                num = i
+            elif "/" in i:
+                self.train_list[num] = text_aux
+            else:
+                text_aux += " " + i
+        f = open("train_list.txt", "w")
+        f.write(json.dumps(self.train_list))
+        f.close()
+        self.n_total = len(self.train_list.keys())
+
+    def implementation(self) -> dict:
+        """implementation
+
+            Funcion que carga los datos, los tokeniza y los procesa con el algoritmo SimHash,
+            devolviendo un diccionario con los resultados 
+
+        Parameters:
+
+        Output: dict
+        """
+        print("Index")
+
+        # Aqui tengo {pal : idf} : {ID_Doc : tf}
+        self.id_pal_cont = {self.TFMethod(
+            bag_w.BagOfWords(text=v)): {k: self.pal_tf} for k, v in self.train_list.items()}
+
+        print("a")
+
+    def count(self, pal) -> dict:
+        cnt = Counter()
+        for _, v in self.train_list.items():
+            if pal in v:
+                cnt[pal] += 1
+
+        return cnt
+
+    def IDFMethod(self) -> int:
+
+        pal_key = self.id_pal_cont.keys()
+
+        pal_value = self.id_pal_cont.values()
+
+        pal_idf = {k: math.log(self.n_total / self.count(v))
+                   for k, v in pal_key.items()}
+        
+        self.index = {pal_idf : pal_value}
+
+
+    def TFMethod(self, item: bag_w.BagOfWords) -> int:
+        # Aqui tengo palabra : TF
+        denominador = sum(item.values())
+        self.pal_tf = [v/denominador for _, v in item.items()]
+        return {k: 0 for k, _ in item.items()}
+
+    def load(self):
+        try:
+            self.index = pickle.load(open(self.filename, 'rb'))
+        except:
+            pass
+
+    def save(self):
+        pickle.dump(self.index, open(self.filename, 'wb'))
+
+    def add(self, key, value):
+        if key in self.index:
+            self.index[key].append(value)
+        else:
+            self.index[key] = [value]
+
+    def get(self, key):
+        if key in self.index:
+            return self.index[key]
+        else:
+            return []
+
+    def __str__(self):
+        return str(self.index)
+
+    def __repr__(self):
+        return str(self.index)
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __setitem__(self, key, value):
+        self.add(key, value)
+
+    def __contains__(self, key):
+        return key in self.index
+
+    def __len__(self):
+        return len(self.index)
+
+    def __iter__(self):
+        return iter(self.index)
--- main.py
+++ main.py
@@ -9,6 +9,7 @@
 import bag_og_words as bag
 import sim_hash as simhash
 import argparse
+import index
 
 
 def parse_args() -> argparse.Namespace:
@@ -21,8 +22,8 @@ def parse_args() -> argparse.Namespace:
 
     parser = argparse.ArgumentParser(
         description='Similitud entre hashes, textos cuasi-duplicados, para la asignatura SIW')
-    parser.add_argument("--file", help="Fichero donde extraera los textos a consultar. Default: ./data/articles_1000.train",
-                        type=str, default="./data/articles_1000.train")
+    parser.add_argument("--file", help="Fichero donde extraera los textos a consultar. Default: ./data/doc-text",
+                        type=str, default="./data/doc-text")
     parser.add_argument(
         "-r", help="El numero de restrictiveness que se quiera aplicar. Default: 10", type=int, default=10)
     parser.add_argument(
@@ -50,10 +51,7 @@ if __name__ == "__main__":
     """
     presentacion()
     # bag.bag_of_words()
-    sim_hash = simhash.SimHash(parse_args=parse_args())
-    f = open("./docs/resultados.txt", "w")
-    f.write(str(sim_hash))
-    f.close()
-    f = open("./docs/resultados_2.txt", "w")
-    f.write(str(sim_hash.score))
-    f.close()
+    ind = index.Index(parse_args())
+    ind.implementation()
+    ind.IDFMethod()
+    
